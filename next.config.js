608942ad32e1173d5101dcdb7c3f4cc1fca575e3
Fr0ntcoder <jambox228@gmail.com>
2023-01-28 23:12:30 +0800

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	sassOptions: {
+		includePaths: ['./src'],
+		prependData: `@import "@/assets/styles/variables/index.scss";`
+	}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
